1d93fc070f61785fd24a940bced96caf8a0bfc9e
Noor <159580993+light03s@users.noreply.github.com>
2026-05-03 10:52:20 -0600

    Update path-finder.py

--- path-finder.py
+++ path-finder.py
@@ -1,3 +1,8 @@
+import curses
+from curses import wrapper
+import queue
+import time
+
 maze = [
     ["#", "#", "#", "#", "#", "#", "#", "#", "#", "O", "#", "#"],
     ["#", "X", " ", " ", "#", " ", " ", " ", "#", " ", "#", "#"],
@@ -11,3 +16,16 @@ maze = [
     ["#", " ", " ", " ", " ", " ", "#", " ", " ", " ", "#", "#"],
     ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"]
 ]
+
+# function used to output onto terminal
+def main(stdscr): 
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
+    color_theme = curses.color_pair(1)
+
+    stdscr.clear()
+    stdscr.addstr(10, 10, "Guten Tag!", color_theme) # when adding a astring need to add the position we are adding the string at
+    stdscr.refresh()
+    stdscr.getch() # wait for user input something before exiting the program
+
+wrapper(main) # this initializes the curses module
+    
