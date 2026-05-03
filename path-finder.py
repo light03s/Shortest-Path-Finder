90f58c65baa214f23a52e90ea16dafb7dd3d2e11
Noor <159580993+light03s@users.noreply.github.com>
2026-05-03 13:31:44 -0600

    can now print the maze

--- path-finder.py
+++ path-finder.py
@@ -4,26 +4,36 @@ import queue
 import time
 
 maze = [
-    ["#", "#", "#", "#", "#", "#", "#", "#", "#", "O", "#", "#"],
-    ["#", "X", " ", " ", "#", " ", " ", " ", "#", " ", "#", "#"],
-    ["#", "#", "#", " ", "#", " ", "#", " ", "#", " ", "#", "#"],
-    ["#", " ", " ", " ", " ", " ", "#", " ", " ", " ", "#", "#"],
-    ["#", " ", "#", "#", "#", " ", "#", "#", "#", " ", "#", "#"],
-    ["#", " ", "#", " ", " ", " ", " ", " ", "#", " ", "#", "#"],
-    ["#", " ", "#", " ", "#", "#", "#", " ", "#", " ", "#", "#"],
-    ["#", " ", " ", " ", "#", " ", " ", " ", "#", " ", "#", "#"],
-    ["#", "#", "#", " ", "#", " ", "#", "#", "#", " ", "#", "#"],
-    ["#", " ", " ", " ", " ", " ", "#", " ", " ", " ", "#", "#"],
-    ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"]
+    ["#", "X", "#", "#", "#", "#", "#", "O", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", "#", "#", " ", "#", " ", "#"],
+    ["#", " ", " ", "#", " ", " ", " ", " ", "#"],
+    ["#", "#", " ", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", " ", " ", " ", "#", " ", " ", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "#", "#"]
 ]
+
+def printMaze(maze, stdscr, path=[]):
+    GREEN = curses.color_pair(1)
+    MAGENTA = curses.color_pair(2)
+
+    for i, row in enumerate(maze): # enumerate returns the index and value in the maze
+        for j, value in enumerate(row):
+            stdscr.addstr(i, j*3, value, GREEN)
+
 
 # function used to output onto terminal
 def main(stdscr): 
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
     color_theme = curses.color_pair(1)
 
     stdscr.clear()
-    stdscr.addstr(10, 10, "Guten Tag!", color_theme) # when adding a astring need to add the position we are adding the string at
+    printMaze(maze, stdscr)
     stdscr.refresh()
     stdscr.getch() # wait for user input something before exiting the program
 
