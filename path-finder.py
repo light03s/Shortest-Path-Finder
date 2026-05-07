61f1f9af7964a35d26d052fe2c68e5eb7ca8bd91
Noor <159580993+light03s@users.noreply.github.com>
2026-05-07 11:58:30 -0600

    add function to find neighbors

--- path-finder.py
+++ path-finder.py
@@ -22,6 +22,33 @@ def findPath(maze, stdscr):
     start = "O"
     end = "X"
 
+    q = queue.Queue()
+    q.put((start_pos, [start_pos])) # use a tuple to store the currentposition and the path to it in the list
+
+    visited = set() # contains all visited positions
+
+    while not q.empty():
+        current_pos, path = q.get() # get the most recent element from the queue
+        row, col = current_pos
+
+        if maze[row][col] == end:
+            return path
+        
+    # if we have not reached the end then we check for neighbors
+    neighbors = findNeighbors(maze, row, col)
+    for neighbor in neighbors:
+        if neighbor in visited: # do not process spaces that are already visited
+            continue
+        
+        r, c = neighbor
+        if maze[r][c] == "#": # not not process #
+            continue
+        
+        # if the neighbor is not visited and is not a # add it to the path and add it to the queue
+        new_path = path + [neighbor]
+        q.put((neighbor, new_path))
+        visited.add(neighbor)
+
 # depth-first search - gaurunteed to find a solution
 
 # find the coordinates of the initial position
@@ -32,6 +59,20 @@ def findStart(maze, start):
                 return i,j # return the position of O
             
     return None # when position does not = O
+
+def findNeighbors(maze, row, col): # make sure the neighbors are a valid next position in the maze
+    neighbors = []
+
+    if row > 0: # go up
+        neighbors.append((row - 1, col))
+    if row + 1 < len(maze): # go down
+        neighbors.append((row + 1, col))
+    if col > 0: # go left
+        neighbors.append((row, col - 1 ))
+    if col + 1 < len(maze[0]): # go right
+        neighbors.append((row, col + 1 ))
+
+    return neighbors
 
 def printMaze(maze, stdscr, path=[]):
     GREEN = curses.color_pair(1)
